191488401e913001260051d8d1972d67dbd9d6c8
spitchell <mr.spitchell@gmail.com>
2019-04-09 23:32:41 +0100

    removed all unnecessary stuff and updated gitignore

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,9 @@
 const tailwindcss = require('tailwindcss')
-const cssnano = require('cssnano')
 const autoprefixer = require('autoprefixer')
-const purgecss = require('@fullhuman/postcss-purgecss')
-
 
 module.exports = {
   plugins: [
     tailwindcss('./tailwind.js'),
-    require('cssnano')({
-      preset: ['default', {
-          discardComments: {
-              removeAll: true,
-          },
-      }]
-  }),
-  purgecss({
-      content: ['./index.html']
-    }),
-
     autoprefixer
   ]
 }
